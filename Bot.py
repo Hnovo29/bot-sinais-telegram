311650137c24f04b16f67698766145c94a4c664d
Hnovo29 <hnovo7469@gmail.com>
2025-04-08 21:33:31 +0100

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+# Carrega os dados das variáveis de ambiente
+TOKEN = os.getenv("7785668664:AAGsUfDE35zeVx54SUBvG6sSL8sJwF2EWWM")
+CHAT_ID = os.getenv("-1002673363905")
+
+# Mensagem de exemplo
+mensagem = "Bot de sinais iniciado com sucesso!"
+
+# Envia a mensagem para o canal
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+payload = {
+    "chat_id": CHAT_ID,
+    "text": mensagem
+}
+
+response = requests.post(url, data=payload)
+
+if response.status_code == 200:
+    print("Mensagem enviada com sucesso!")
+else:
+    print("Erro ao enviar mensagem:", response.text)
