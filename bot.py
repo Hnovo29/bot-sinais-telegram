e71f7e65f34b9d52627c9ef67b6d54cc687cc425
Hnovo29 <hnovo7469@gmail.com>
2025-04-08 22:04:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-import os
 import requests
 
-# Carrega os dados das variáveis de ambiente
-TOKEN = os.getenv("7785668664:AAGsUfDE35zeVx54SUBvG6sSL8sJwF2EWWM")
-CHAT_ID = os.getenv("-1002673363905")
+# Coloque seus dados diretamente aqui
+TOKEN = "7785668664:AAGsUfDE35zeVx54SUBvG6sSL8sJwF2EWWM"
+CHAT_ID = "-1002673363905"
 
 # Mensagem de exemplo
 mensagem = "Bot de sinais iniciado com sucesso!"
